4a7018f50ee0e6458d31f0b1e398fb859588a910
= <=>
2025-11-12 14:39:35 +0700

    ver5 - can login

--- app/src/main/java/com/example/login_app/MainActivity.kt
+++ app/src/main/java/com/example/login_app/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.login_app
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val edtUsn = findViewById<EditText>(R.id.edtUsn)
+        val edtPass = findViewById<EditText>(R.id.edtPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = edtUsn.text.toString()
+            val password = edtPass.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                val intent = Intent(this, Profile::class.java)
+                intent.putExtra("username", username)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "กรุณากรอกชื่อก่อนเข้าสู่ระบบ", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/login_app/Profile.kt
@@ -0,0 +1,12 @@
+package com.example.login_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+    }
+}
